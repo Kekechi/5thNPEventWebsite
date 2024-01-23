27556b52b769305fdfaed69ad7cd719a65920cc3
Keigo Morita <56617711+Kekechi@users.noreply.github.com>
2024-01-23 13:18:42 -0800

    Create Schedule.jsx

---
+++ src/Components/Home/Schedule/Schedule.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+function Schedule() {
+  return (
+    <div className="schedule">
+      <div className="section-anchor" id="schedule"></div>
+      <Container className="py-5">
+        <h1 className="text-light">日時</h1>
+      </Container>
+    </div>
+  );
+}
+
+export default Schedule;
