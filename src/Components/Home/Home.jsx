bb31642cb5601a84dab2be36381ea549795d3372
Keigo Morita <56617711+Kekechi@users.noreply.github.com>
2024-01-23 16:07:23 -0800

    add sections

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./Hero/Hero";
 import Schedule from "./Schedule/Schedule";
+import Sections from "./Sections/Sections";
 
 function Home() {
   return (
     <main id="top">
       <Hero />
       <Schedule />
+      <Sections />
     </main>
   );
 }
