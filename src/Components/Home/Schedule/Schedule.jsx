3635be942d956cab0e26a7fada34a611942a0486
Keigo Morita <56617711+Kekechi@users.noreply.github.com>
2024-01-23 16:06:51 -0800

    Update Schedule.jsx

--- src/Components/Home/Schedule/Schedule.jsx
+++ src/Components/Home/Schedule/Schedule.jsx
@@ -6,7 +6,7 @@ function Schedule() {
     <div className="schedule">
       <div className="section-anchor" id="schedule"></div>
       <Container className="py-5">
-        <h1 className="text-light">日時</h1>
+        <h1 className="text-light">開催概要</h1>
       </Container>
     </div>
   );
