559b45150d1e91713518f319fb474b05ac84c108
Keigo Morita <56617711+Kekechi@users.noreply.github.com>
2024-01-23 16:06:42 -0800

    add 登録ボタン

--- src/Components/Home/Hero/Hero.jsx
+++ src/Components/Home/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import newspicksImg from "../../../Assets/newspicks.png";
 
 function Hero() {
@@ -11,10 +11,12 @@ function Hero() {
             <img src={newspicksImg} alt="" className="hero-pic" />
           </Col>
           <Col xs={12} md={8}>
+            <h4 className="text-warning">3月中旬2024</h4>
             <h1 className="text-light title">
               第五期Student Picker オンラインイベント告知用ウェブサイト
             </h1>
             <p className="text-light headline"></p>
+            <Button variant="outline-warning">登録フォーム</Button>
           </Col>
         </Row>
       </Container>
